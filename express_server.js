05d51834e817b7c8753af5dab40a7605bd86c255
Niki Jiandani <niki.jiandani@gmail.com>
2019-06-04 23:18:42 +0000

    Install & add ejs & set it as the view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+//set viewing engine
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
