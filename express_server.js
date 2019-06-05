ba0840d01e38fcfa93598ad6332eacddc2a2d15a
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-04 21:36:32 -0400

    Add generateRandomString function to express server

--- express_server.js
+++ express_server.js
@@ -13,6 +13,10 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
