c12f3d7036cbaf81c53a274ff000137389fea0e3
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-04 20:05:03 -0400

    Add another page to display a single URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -25,7 +25,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
