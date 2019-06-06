f0662476149ff343669662fb72b785060db8e205
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-06 00:55:36 -0400

    Add register route

--- express_server.js
+++ express_server.js
@@ -35,6 +35,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_register", templateVars);
+})
+
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     username: req.cookies["username"],
